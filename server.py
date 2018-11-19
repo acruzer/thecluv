0091d70e37876345762a7cba12045c353ac957d9
areliscruz <areliscs@gmail.com>
2018-11-18 16:41:34 -0800

    Debug status update

--- server.py
+++ server.py
@@ -361,7 +361,8 @@ def delete_img_aws(article_obj):
 if __name__ == "__main__":
 	# We have to set debug=True here, since it has to be True at the
 	# point that we invoke the DebugToolbarExtension
-	app.debug = True
+	app.debug = False
+	
 	# DEBUG_TB_INTERCEPT_REDIRECTS = False
 	# make sure templates, etc. are not cached in debug mode
 	app.jinja_env.auto_reload = app.debug
